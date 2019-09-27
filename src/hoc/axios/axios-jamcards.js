2bc9144335808d04b6d74699223a56c4a2cd1123
Raghuram Subramanian <raghulaihos@gmail.com>
2019-09-27 15:22:01 -0700

    Added server api url

--- src/hoc/axios/axios-jamcards.js
+++ src/hoc/axios/axios-jamcards.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:'http://localhost:5000'
+    baseURL:'https://jammer-server.herokuapp.com'
 });
 
 export default instance;
