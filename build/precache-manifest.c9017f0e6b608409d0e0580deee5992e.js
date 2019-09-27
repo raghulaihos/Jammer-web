39f4426cd4ffaff0a3a743f9cbb12621de2835a7
Raghuram Subramanian <raghulaihos@gmail.com>
2019-09-27 14:00:39 -0700

    Added build folder

---
+++ build/precache-manifest.c9017f0e6b608409d0e0580deee5992e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bffc8757ac357845085900289fa0ee19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6288eda7e766b4a16b6a",
+    "url": "/static/css/2.4b14b85e.chunk.css"
+  },
+  {
+    "revision": "4061c845042ff3665efb",
+    "url": "/static/css/main.e55ac7d9.chunk.css"
+  },
+  {
+    "revision": "6288eda7e766b4a16b6a",
+    "url": "/static/js/2.f1875ded.chunk.js"
+  },
+  {
+    "revision": "4061c845042ff3665efb",
+    "url": "/static/js/main.46152f43.chunk.js"
+  },
+  {
+    "revision": "9bf0e147d4ad480bf0cd",
+    "url": "/static/js/runtime~main.1fc4146f.js"
+  },
+  {
+    "revision": "be6b9a35aac1b6a43ab1e2526800f95f",
+    "url": "/static/media/enter-sandman.be6b9a35.mp4"
+  },
+  {
+    "revision": "6984cb0e4a302171660cc3f0c14a5a2e",
+    "url": "/static/media/jamroom1.6984cb0e.jpg"
+  },
+  {
+    "revision": "6e143cda044f10bbb170b2ed76f5a097",
+    "url": "/static/media/jamroom2.6e143cda.jpg"
+  },
+  {
+    "revision": "279303f0bf5d6f81aab64ec55abfb9d0",
+    "url": "/static/media/jamroom3.279303f0.jpg"
+  },
+  {
+    "revision": "092582dc46452ebbe0f567d76396a18e",
+    "url": "/static/media/jamroom4.092582dc.jpg"
+  },
+  {
+    "revision": "cb71e9535c1061adb795f7896eedad98",
+    "url": "/static/media/logo.cb71e953.jpg"
+  },
+  {
+    "revision": "11ef92647cbca7b4fbeb62035cc47423",
+    "url": "/static/media/sound.11ef9264.png"
+  }
+]);
